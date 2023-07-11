6b6868133c4372edb00eed2bc8328008b646de06
naokiupa <naokiupa@gmail.com>
2023-07-11 18:30:00 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,17 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+  <div class="talk">
+    <div class="chara">
+      <!-- <img :src="require('../assets/charactor/smile.png')" /> -->
+      <img :class="[{'charaImg': true},{'updown': isShow2}]" src="../assets/charactor/smile.png">
+    </div>
+    <p :class="[{'chat': true},{'isShow': isShow}]">
+おはよ
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="input-chat">
+      <input
+      v-model="text"
+      @input="event">
+    </div>
   </div>
 </template>
 
@@ -35,24 +20,115 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      emotion: 'smile',
+      text: "",
+      // chat: "chat",
+      isShow: false,
+      isShow2: false,
+    }
+  },
+  methods: {
+    event: function() {
+      // alert(value);
+      if (this.text == "おはよ") {
+        this.isShow = true;
+        this.isShow2 = true;
+        setTimeout(() => {
+          this.isShow2 = false;}
+        ,3000
+      )
+        console.log(this.text);
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.talk {
+  background-image: url("../assets/_living_3.jpg");
+  background-repeat: no-repeat;
+    background-size: cover;
+    background-position-x: right;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top:0;
+  right:0;
+  bottom:0;
+  left:0;
+  overflow: hidden;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.charaImg {
+  position: absolute;
+    right: 20px;
+    bottom: 0;
+    height: 66vh;
 }
-li {
+.chat {
   display: inline-block;
-  margin: 0 10px;
+    position: absolute;
+    bottom: -30px;
+    right: 24px;
+    padding: 5px 25px;
+    background: #ffffff7a;
+    border-radius: 8px;
+    border: 1px solid #a5a5a5;
+    transition: all 7s 1s ease-in-out;
+    /* opacity: 0; */
 }
-a {
-  color: #42b983;
+.chat.isShow {
+  /* opacity: 1; */
+  bottom: 100%;
+}
+.input-chat {
+  position: absolute;
+    bottom: 12px;
+    right: 0;
+    left: 0;
+    padding: 8px;
+    margin: auto;
+    width: 80%;
+    height: 80px;
+    background: #ffffff80;
+    border-radius: 20px;
+}
+.input-chat input {
+
+  width: 86%;
+    border-radius: 12px;
+    border: none;
+    height: 22px;
+    margin-top: 4px;
+
+}
+
+.updown {
+    animation-name:updown1;   /* アニメーション名の指定 */
+    animation-delay:0s;   /* アニメーションの開始時間指定 */
+    animation-duration: 3s;   /* アニメーション動作時間の指定 */
+    animation-timing-function: ease-in-out;  /* アニメーションの動き指定（徐々に早く）*/
+    animation-iteration-count: infinite; 
+}
+@keyframes updown1 {
+  0% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  65% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(0);
+  }
 }
 </style>
